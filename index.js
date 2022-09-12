a87f54310eb46264096da65d54aeebd4eefdc862
Abdul Amaan <99074207+Abdul7Amaan7@users.noreply.github.com>
2022-09-12 18:31:22 +0530

    upgrading my website in future

---
+++ index.js
@@ -0,0 +1,8 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('bm'),
+    renderer: 'svg',
+    loop: false,
+    autoplay: true,
+    path: 'https://assets1.lottiefiles.com/packages/lf20_weml3y4m.json'
+  })
+  
